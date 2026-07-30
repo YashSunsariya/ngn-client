03dec8bed530b3000383c5256ca36f06c444d9fd
Yash Sunsariya <yashsunsariya2005@gmail.com>
2026-07-30 13:11:25 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
-        <Toaster position="center" toastOptions={{ duration: 3000 }} />
+        <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
       </BrowserRouter>
     </Provider>
   </StrictMode>,
